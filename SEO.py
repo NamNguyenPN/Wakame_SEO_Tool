024b212da4e7af0bf6af5612ab1445921129f476
NamNguyenPN <34613861+NamNguyenPN@users.noreply.github.com>
2021-05-26 10:46:15 +0700

    Add files via upload

---
+++ SEO.py
@@ -0,0 +1,167 @@
+
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+from selenium.webdriver.common.keys import Keys
+from selenium.webdriver.support.ui import WebDriverWait as WDW
+from selenium.webdriver.chrome.service import Service
+import time
+import random as rd
+
+class SEO:
+    def __init__(self):
+        self.driver=None
+        self.home_path=None
+        
+    def delay(self,times):
+        time.sleep(times)
+    def edge(self,object):
+        self.driver=webdriver.Edge(executable_path="msedgedriver.exe")
+        self.driver.maximize_window()
+        self.driver.get("https://www.google.com")
+    def chrome(self,object):            
+        self.driver=webdriver.Chrome()
+        self.driver.maximize_window()
+        if object == None:
+            self.driver.get("https://www.google.com")
+        else:
+            self.driver.get(object)
+    def back(self):
+        
+        self.driver.back()        
+    def quit(self):
+        if(self.driver!=None):
+            self.driver.quit()
+        self.driver=None
+    def get_driver(self):
+        return self.driver
+        
+        
+    def search(self,key_words):
+        self.driver.implicitly_wait(300)
+        search_box=self.driver.find_element(By.NAME,'q')
+        search_box.click()
+        for letter in key_words:
+            search_box.send_keys(letter)
+            speed=rd.uniform(0,1.5)
+            self.delay(speed)
+        search_box.send_keys(Keys.ENTER)
+    def sc_down(self):
+        self.driver.implicitly_wait(300)
+        height=self.driver.execute_script("return document.body.scrollHeight")
+        for i in range(0,height):
+            script="window.scrollTo(0,"+str(i)+")"
+            self.driver.execute_script(script)
+    def sc_up(self):
+        self.driver.implicitly_wait(300)
+        height=self.driver.execute_script("return document.body.scrollHeight")
+        for i in range(0,height):
+            script="window.scrollTo(0,"+str(height-i)+")"
+            self.driver.execute_script(script)
+    def rank(self):
+        for i in range(1,21):
+            if(i%10!=0):
+                xpath='//*[@id="rso"]/div['+str(i%10)+']'
+            else:
+                xpath='//*[@id="rso"]/div['+str(10)+']'
+            try:
+                cite_tag=self.driver.find_element(By.XPATH,xpath+'//cite')
+                if "wakame-uit.online" in cite_tag.text:
+                    print("Top",i,"trong tìm kiếm của Google")
+                    wakame=self.driver.find_element(By.XPATH,xpath+'//a')
+                    return i,wakame
+                if (i%10==0 and i<20):
+                    n_p='//*[@id="pnnext"]'
+                    next=self.driver.find_element(By.XPATH,n_p)
+                    next.click()                
+            except:
+                print("Failed")
+            
+        print("Ngoài top 10 trong tìm kiếm Google")
+        return None,None
+    def toPage(self):
+        self.driver.implicitly_wait(10)
+        index,page=self.rank()
+        if(page != None):
+            page.click()
+        else:
+            print("ToPage Failed")
+            return 1/0
+            
+    def toTop(self):
+        self.driver.implicitly_wait(100)
+        back_top_path='//*[@id="back-top"]/a'
+        back_top=self.driver.find_element(By.XPATH,back_top_path)
+        back_top.click()
+        
+    def read(self):
+        try:
+            self.driver.implicitly_wait(100)
+            height=self.driver.execute_script("return document.body.scrollHeight")
+            for i in range(0,height):
+                script="window.scrollTo(0,"+str(i)+")"
+                self.driver.execute_script(script)
+                self.delay(0.08)
+        except :
+            pass
+
+    def Home(self):
+        try:
+            self.driver.implicitly_wait(300)
+            home_path='//*[@id="navigation"]/li[1]/a'
+            home=self.driver.find_element(By.XPATH,home_path)
+            home.click()
+        except :
+            pass
+    
+    def News(self):
+        try:
+            self.driver.implicitly_wait(300)
+            news_path="//*[@id='navigation']/li[3]/a"
+            news=self.driver.find_element(By.XPATH,news_path)
+            news.click()
+        except :
+            print("To News Fail")
+            pass
+    
+    def search_box(self,key_words):
+        try:
+            search_box_path="/html/body/main/section[2]/div/div/div[2]/div/aside[1]/form/div/div/input"
+            search_box=self.driver.find_element(By.XPATH,search_box_path)
+            search_box.click()
+            for letter in key_words:
+                search_box.send_keys(letter)
+                speed=rd.uniform(0,1.5)
+                self.delay(speed)
+            search_box.send_keys(Keys.ENTER)
+        except :
+            pass
+        
+    def toContent(self,num):
+        n=rd.randint(1,num)
+        if(n%4!=0):
+            p='/html/body/main/section[2]/div/div/div[1]/div/article['+str(n%4)+']/div[2]/a'
+        else:
+            p='/html/body/main/section[2]/div/div/div[1]/div/article[4]/div[2]/a'
+        try:
+            h=self.driver.find_element_by_xpath(p)  
+            h.click()         
+        except:
+            print("To Contetn Faild")
+            pass
+        
+    def Courses(self):
+        self.driver.implicitly_wait(300)
+        courses_path='//*[@id="navigation"]/li[2]/a'
+        courses=self.driver.find_element(By.XPATH,courses_path)
+        courses.click()
+    
+    def About(self):
+        try:
+            self.driver.implicitly_wait(300)
+            aboutus_path='//*[@id="navigation"]/li[4]/a'
+            aboutus=self.driver.find_element(By.XPATH,aboutus_path)
+            aboutus.click()
+        except:
+            print("To About Fail")
+            pass
+        
---
+++ change_ip_mac.py
@@ -0,0 +1,176 @@
+
+
+import subprocess, winreg, re, time, wmi
+import netifaces
+import random as rd
+import urllib.request
+
+class IP():
+    def __init__(self):
+        self.gws=netifaces.gateways()
+        self.nic_configs=wmi.WMI().Win32_NetworkAdapterConfiguration(IPEnabled=True)
+        self.nic=self.nic_configs[0]
+        self.gateway=self.gws['default'][netifaces.AF_INET][0]
+        self.newIP=self.gateway.split(".")
+        self.newIP[-1]=str(rd.randint(5,150))
+        self.newIP=".".join(self.newIP)
+        self.subnetmask=u"255.255.255.0"
+        self.DNS=['208.67.222.222','208.67.220.220']
+    def change_IP(self):        
+        try:
+            self.nic.EnableStatic(IPAddress=[self.newIP],SubnetMask=[self.subnetmask])
+            self.nic.SetGateways(DefaultIPGateway=[self.gateway])
+            self.nic.SetDNSServerSearchOrder(DNSServerSearchOrder = self.DNS)
+            print("Chuyển đổi IP thành công. IP mới:",self.newIP)
+        except:
+            print("Chuyển đổi IP thất bại")
+        time.sleep(10)
+    def show_IP(self):
+        for i in netifaces.interfaces():
+            try:
+                if(netifaces.ifaddresses(i)[netifaces.AF_INET][0]['addr']!="127.0.0.1"):
+                    print("IP Address: ", netifaces.ifaddresses(i)[netifaces.AF_INET][0]['addr'])
+                    print("Mask: ", netifaces.ifaddresses(i)[netifaces.AF_INET][0]['netmask'])
+                    print("Gateway: ", netifaces.gateways()['default'][netifaces.AF_INET][0])
+
+            except:pass
+
+
+
+class MAC():
+    def __init__(self):
+        self.list_2letter=["0A","0E","02","06"]
+        self.other_letter="".join([str(rd.randint(0,9)) for i in range(10)])
+        self.genMAC=self.list_2letter[rd.randint(0,3)]+self.other_letter
+        gws = netifaces.gateways()
+        self.TN=gws['default'][list(gws['default'].keys())[0]][1]
+        self.macAddRegex =re.compile(r"([A-Za-z0-9]{2}[:-]){5}([A-Za-z0-9]{2})")
+        self.transportName =re.compile("({.+})")
+        self.adapterIndex =re.compile("([0-9]+)")
+        self.getmac_output=subprocess.run("getmac",capture_output=True).stdout.decode().split('\n')
+        self.mac_addresses=[]
+        for macAdd in self.getmac_output:
+            macFind = self.macAddRegex.search(macAdd)
+            transportFind=self.transportName.search(macAdd)
+            if macFind == None or transportFind == None:
+                continue
+            self.mac_addresses.append((macFind.group(0),transportFind.group(0)))
+    def show_MAC(self):
+        for index,item in enumerate(self.mac_addresses):
+            print(f"{index} - Địa chỉ MAC: {item[0]} - Transport Name: {item[1]}")
+
+    def change_MAC(self):
+        controller_key_part=r"SYSTEM\ControlSet001\Control\Class\{4d36e972-e325-11ce-bfc1-08002be10318}"
+        with winreg.ConnectRegistry(None,winreg.HKEY_LOCAL_MACHINE) as hkey:
+            controller_key_folders =[("\\000"+str(item) if item < 10 else "\\00"+str(item))for item in range(0,21)]
+            for key_folder in controller_key_folders:
+                try:
+                    with winreg.OpenKey(hkey,controller_key_part+key_folder,0,winreg.KEY_ALL_ACCESS) as regkey:
+                        try:
+                            count=0
+                            while True:
+                                name,value,type=winreg.EnumValue(regkey,count)
+                                count=count+1
+                                if name=="NetCfgInstanceId" and value==self.mac_addresses[0][1]:
+                                    new_mac_address = self.genMAC
+                                    winreg.SetValueEx(regkey, "NetworkAddress", 0, winreg.REG_SZ, new_mac_address)
+                                    print("Sửa địa chỉ MAC trên Registry thành công: {","-".join([(self.genMAC[i:i+2]) for i in range(0, len(self.genMAC), 2)]),"}")
+                                    break
+                        except:
+                            pass
+                except:
+                    pass
+
+    def restart_Wifi(self):
+        run_last_part=True
+        while run_last_part:
+            network_adapters = subprocess.run(["wmic", "nic", "get", "name,index"], capture_output=True).stdout.decode('utf-8', errors="ignore").split('\r\r\n')
+            for adapter in network_adapters:
+                adapter_index_find = self.adapterIndex.search(adapter.lstrip())
+                if adapter_index_find and "Wireless" in adapter:
+                    disable = subprocess.run(["wmic", "path", "win32_networkadapter", "where", f"index={adapter_index_find.group(0)}", "call", "disable"],capture_output=True)
+                    if(disable.returncode == 0):
+                        print("Vô hiệu hóa",adapter.lstrip())
+                    enable = subprocess.run(["wmic", "path", f"win32_networkadapter", "where", f"index={adapter_index_find.group(0)}", "call", "enable"],capture_output=True)
+                    if (enable.returncode == 0):
+                        print("Kích hoạt",adapter.lstrip())
+                getmac_output = subprocess.run("getmac", capture_output=True).stdout.decode()
+                mac_add = "-".join([(self.genMAC[i:i+2]) for i in range(0, len(self.genMAC), 2)])
+                if mac_add in getmac_output:
+                    print("Chuyển đổi địa chỉ MAC thành công.\nĐịa chỉ MAC mới: {","-".join([(self.genMAC[i:i+2]) for i in range(0, len(self.genMAC), 2)]),"}")
+                    run_last_part=False
+                    break
+            break
+
+
+class IP_MAC(MAC,IP):
+    def __init__(self):
+        self.ip=IP()
+        self.mac=MAC()
+    def run(self):
+        try:
+            self.mac.change_MAC()
+            self.mac.restart_Wifi()
+        except:
+            print("Chuyển địa chỉ MAC thất bại")
+            
+        flag=True
+        count=0
+        while(flag):
+            for i in netifaces.interfaces():
+                try:
+                    if(netifaces.ifaddresses(i)[netifaces.AF_INET][0]['addr']!="127.0.0.1"):
+                        print("Wifi is connected")
+                        flag=False
+                        break
+                except:
+                    pass
+            count=count+1
+            if(count==2000):
+                flag=False
+        try:
+            time.sleep(1)
+            urllib.request.urlopen('https://www.google.com')
+            print("Internet has connected")
+        except:
+            pass
+            
+        try:
+            self.ip.change_IP()
+            self.ip.show_IP()
+        except:
+            print("Chuyển IP thất bại")
+        
+
+        flag=True
+        count=0
+        while(flag):
+            for i in netifaces.interfaces():
+                try:
+                    if(netifaces.ifaddresses(i)[netifaces.AF_INET][0]['addr']!="127.0.0.1"):
+                        print("Wifi is connected")
+                        flag=False
+                        break
+                except:
+                    pass
+            count=count+1
+            if(count==2000):
+                flag=False
+        try:
+            time.sleep(1)
+            urllib.request.urlopen('https://www.google.com')
+            print("Internet has connected")
+        except:
+            print(1/0)
+    def get_IP(self):
+        return self.ip.newIP
+    def get_MAC(self):
+        return "-".join([(self.mac.genMAC[i:i+2]) for i in range(0, len(self.mac.genMAC), 2)])
+    def is_connect(self):
+        try:
+            time.sleep(1)
+            urllib.request.urlopen('https://www.google.com')
+            return True
+        except:
+            print("No Internet")
+            return 1/0
